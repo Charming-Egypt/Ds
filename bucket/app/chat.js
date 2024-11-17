20fcc45e469780195e2afc4712181a611e873ec8
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2024-11-17 08:09:55 +0200

    Update chat.js

--- bucket/app/chat.js
+++ bucket/app/chat.js
@@ -25,3 +25,34 @@
  const db = getDatabase();
  const auth = getAuth(app);
 
+
+
+        function Gettemples() {
+            const dbref = ref(db);
+            get(child(dbref, 'temples')).then((snapshot) => {
+                const templesContainer = document.getElementById('temples-container');
+                snapshot.forEach((templeSnapshot) => {
+                    const templeData = templeSnapshot.val();
+                    const templeElement = createtempleElement(templeData.name_en, templeData.photo, templeData.blog_url);
+                    templesContainer.appendChild(templeElement);
+                });
+            });
+        }
+
+        function createtempleElement(name, imageUrl, pageUrl) {
+            const templeDiv = document.createElement('div');
+            templeDiv.className = 'temple';
+            templeDiv.style.backgroundImage = `url(${imageUrl})`;
+
+            const nameLink = document.createElement('a');
+            nameLink.className = 'temple-name';
+            nameLink.innerText = name;
+            nameLink.href = pageUrl;
+            nameLink.target = '_self';
+            templeDiv.appendChild(nameLink);
+
+            return templeDiv;
+        }
+ if (window.location.pathname === '/' || window.location.pathname === '/') {
+        window.addEventListener('load', Gettemples);
+    }
