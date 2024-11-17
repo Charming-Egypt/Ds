45499cfe14073c50126270f0ee113a19163be281
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2024-11-17 07:54:25 +0200

    Update chat.js

--- bucket/app/chat.js
+++ bucket/app/chat.js
@@ -1,22 +1,27 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getDatabase, ref, get, set, push, onChildAdded, onValue, update, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+<script type="module">
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+        import { getDatabase, ref, get, set, push, onChildAdded, onValue, update, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+        import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+      
+     
+      
+      
+      
       
 
-const firebaseConfig = {
-      apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
-      authDomain: "egypt-travels.firebaseapp.com",
-      databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
-      projectId: "egypt-travels",
-      storageBucket: "egypt-travels.appspot.com",
-      messagingSenderId: "477485386557",
-      appId: "1:477485386557:web:755f9649043288db819354",
-      measurementId: "G-RKD5F46NM8"
+        const firebaseConfig = {
+            apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
+            authDomain: "egypt-travels.firebaseapp.com",
+            databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
+            projectId: "egypt-travels",
+            storageBucket: "egypt-travels.appspot.com",
+            messagingSenderId: "477485386557",
+            appId: "1:477485386557:web:755f9649043288db819354",
+            measurementId: "G-RKD5F46NM8"
         };
 
         // Initialize Firebase
  const app = initializeApp(firebaseConfig);
  const db = getDatabase();
  const auth = getAuth(app);
-  
+</scrip>
