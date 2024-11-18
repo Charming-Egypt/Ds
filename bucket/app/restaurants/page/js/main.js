94f6fff9d4551f8ec97588353388391c5e1785e3
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2024-11-18 13:27:37 +0200

    Update main.js

--- bucket/app/restaurants/page/js/main.js
+++ bucket/app/restaurants/page/js/main.js
@@ -135,26 +135,6 @@
   </script>
 
   
-  // Import the functions you need from the SDKs you need
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
-        import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
-        import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-
-        const firebaseConfig = {
-            apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
-            authDomain: "egypt-travels.firebaseapp.com",
-            databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
-            projectId: "egypt-travels",
-            storageBucket: "egypt-travels.appspot.com",
-            messagingSenderId: "477485386557",
-            appId: "1:477485386557:web:755f9649043288db819354",
-            measurementId: "G-RKD5F46NM8"
-        };
-
-        const app = initializeApp(firebaseConfig);
-        const db = getDatabase();
-        const auth = getAuth(app);
 
 
 
