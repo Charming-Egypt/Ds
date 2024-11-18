74082052daf3f460f18f508f5df7dc8a54b9b1c7
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2024-11-18 13:19:55 +0200

    Update main.js

--- bucket/app/restaurants/page/js/main.js
+++ bucket/app/restaurants/page/js/main.js
@@ -118,4 +118,43 @@
 })(jQuery);
 
 
-  
+  <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/app/restaurants/page/lib/wow/wow.min.js"></script>
+    <script src="/app/restaurants/page/lib/easing/easing.min.js"></script>
+    <script src="/app/restaurants/page/lib/waypoints/waypoints.min.js"></script>
+    <script src="/app/restaurants/page/lib/counterup/counterup.min.js"></script>
+    <script src="/app/restaurants/page/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="/app/restaurants/page/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="/app/restaurants/page/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="/app/restaurants/page/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="/app/restaurants/page/js/main.js">
+  </script>
+
+  <script type="module">
+  // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+        import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+        import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
+            authDomain: "egypt-travels.firebaseapp.com",
+            databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
+            projectId: "egypt-travels",
+            storageBucket: "egypt-travels.appspot.com",
+            messagingSenderId: "477485386557",
+            appId: "1:477485386557:web:755f9649043288db819354",
+            measurementId: "G-RKD5F46NM8"
+        };
+
+        const app = initializeApp(firebaseConfig);
+        const db = getDatabase();
+        const auth = getAuth(app);
+
+
+
