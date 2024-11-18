a7c5815a81741505123108cdddf99a4983ac5f33
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2024-11-18 11:28:03 +0200

    Update main.js

--- bucket/app/restaurants/page/js/main.js
+++ bucket/app/restaurants/page/js/main.js
@@ -119,27 +119,25 @@
 
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+        import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+        import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
-    authDomain: "egypt-travels.firebaseapp.com",
-    databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
-    projectId: "egypt-travels",
-    storageBucket: "egypt-travels.appspot.com",
-    messagingSenderId: "477485386557",
-    appId: "1:477485386557:web:755f9649043288db819354",
-    measurementId: "G-RKD5F46NM8"
-  };
+        const firebaseConfig = {
+            apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
+            authDomain: "egypt-travels.firebaseapp.com",
+            databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
+            projectId: "egypt-travels",
+            storageBucket: "egypt-travels.appspot.com",
+            messagingSenderId: "477485386557",
+            appId: "1:477485386557:web:755f9649043288db819354",
+            measurementId: "G-RKD5F46NM8"
+        };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+        const app = initializeApp(firebaseConfig);
+        const db = getDatabase();
+        const auth = getAuth(app);
 
 
 
