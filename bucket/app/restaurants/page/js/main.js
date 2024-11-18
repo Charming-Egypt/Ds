11f03ba123f6a5af6fee9216d3c278e8e25c2194
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2024-11-18 13:36:54 +0200

    Update main.js

--- bucket/app/restaurants/page/js/main.js
+++ bucket/app/restaurants/page/js/main.js
@@ -118,16 +118,7 @@
 })(jQuery);
 
 
-  const firebaseConfig = {
-            apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
-            authDomain: "egypt-travels.firebaseapp.com",
-            databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
-            projectId: "egypt-travels",
-            storageBucket: "egypt-travels.appspot.com",
-            messagingSenderId: "477485386557",
-            appId: "1:477485386557:web:755f9649043288db819354",
-            measurementId: "G-RKD5F46NM8"
-        };
+  
 
         
 
