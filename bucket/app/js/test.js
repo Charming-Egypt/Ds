071f8b174bc8ade5875c583dd2a5c4647f012bcc
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2025-05-04 17:14:32 +0300

    Update test.js

--- bucket/app/js/test.js
+++ bucket/app/js/test.js
@@ -42,6 +42,9 @@ const stepIndicators = [
 let currentStep = 0;
 
 // Trip configuration
+const path = window.location.pathname;
+const match = path.match(/\/p\/(.*?)\.html/);
+let tripName = match && match[1] ? match[1] : '';
 let tripsData = {};
 let selectedTripType = "";
 const MAX_PER_TYPE = 10;
