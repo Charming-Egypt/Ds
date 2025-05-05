77e628b7203275168c2ae3cddf6a6ea473540d04
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2025-05-05 10:31:41 +0300

    Update test.js

--- bucket/app/js/test.js
+++ bucket/app/js/test.js
@@ -531,15 +531,15 @@ async function submitForm() {
         merchantId: 'MID-33260-3',
         orderId: refNumber,
         amount: formData.total,
-        currency: formData.currency
+        currency: formData.currency,
         metadata: {
-          customerName: bookingData.username,
-          customerEmail: bookingData.email,
-          customerPhone: bookingData.phone,
-          tripDetails: bookingData.tour,
-          tripDate: bookingData.tripDate,
-          hotel: bookingData.hotelName,
-          room: bookingData.roomNumber
+          kcustomerName: bookingData.username,
+          kcustomerEmail: bookingData.email,
+          kcustomerPhone: bookingData.phone,
+          ktripDetails: bookingData.tour,
+          ktripDate: bookingData.tripDate,
+          khotel: bookingData.hotelName,
+          kroom: bookingData.roomNumber
           }
       }),
     });
@@ -554,16 +554,15 @@ async function submitForm() {
     // Construct Kashier URL with metadata
 
 const kashierUrl = `https://payments.kashier.io/?merchantId=MID-33260-3&orderId=${refNumber}&amount=${formData.total}&currency=${formData.currency}&hash=${data.hash}&mode=live&merchantRedirect=https://www.discover-sharm.com/p/payment-status.html&failureRedirect=false&redirectMethod=get&metadata=${encodeURIComponent(JSON.stringify({
-      customerName: formData.username,
-      customerEmail: formData.email,
+      kcustomerName: formData.username,
+      kcustomerEmail: formData.email,
       tripDetails: `${tripPName} - ${selectedTripType}`,
-      tripDate: formData.tripDate,
-      hotel: formData.hotelName,
-      room: formData.roomNumber,
-      adults: formData.adults,
-      children: formData.childrenUnder12,
-      infants: formData.infants,
-      total: formData.total
+      ktripDate: formData.tripDate,
+      khotel: formData.hotelName,
+      kroom: formData.roomNumber,
+      kadults: formData.adults,
+      kchildren: formData.childrenUnder12,
+      kinfants: formData.infants
     }))}`;
 
     
