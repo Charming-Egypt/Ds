65a8b51555944a6fbc4b067d4262828b48ed1140
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2025-05-17 02:05:24 +0300

    Update trip.js

--- bucket/app/js/trip.js
+++ bucket/app/js/trip.js
@@ -2,19 +2,7 @@
 let swiper;
 let currentVideoSlide = null;
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
-  authDomain: "egypt-travels.firebaseapp.com",
-  databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
-  projectId: "egypt-travels",
-  storageBucket: "egypt-travels.appspot.com",
-  messagingSenderId: "477485386557",
-  appId: "1:477485386557:web:755f9649043288db819354"
-};
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-const auth = firebase.auth();
+
 
 // Global variables
 let tripData = {};
