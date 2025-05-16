fb3905200af9a1a0b05d06f8b5950159db5a40ee
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2025-05-16 10:33:16 +0300

    Update cof.js

--- bucket/app/js/cof.js
+++ bucket/app/js/cof.js
@@ -1,13 +1,9 @@
-
-  const firebaseConfig = {
-      apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
-      authDomain: "egypt-travels.firebaseapp.com",
-      databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
-      projectId: "egypt-travels",
-      storageBucket: "egypt-travels.appspot.com",
-      messagingSenderId: "477485386557",
-      appId: "1:477485386557:web:755f9649043288db819354"
-    };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
+  authDomain: "egypt-travels.firebaseapp.com",
+  databaseURL: "https://egypt-travels-default-rtdb.firebaseio.com",
+  projectId: "egypt-travels",
+  storageBucket: "egypt-travels.appspot.com",
+  messagingSenderId: "477485386557",
+  appId: "1:477485386557:web:755f9649043288db819354"
+};
