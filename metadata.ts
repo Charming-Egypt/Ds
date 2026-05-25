24383157c0391289b778287f123ebde8602d856d
Charming-Egypt <Charming-Egypt@users.noreply.github.com>
2026-05-25 17:31:10 +0000

    update: generated bucket and output Triggered by e5159f63256c66edcad68cac230aad31400ac5b5

--- metadata.ts
+++ metadata.ts
@@ -1,5 +1,5 @@
 /**
- * This is auto generated metadata file, generated at: Tue Mar 18 2025 08:47:33 GMT+0000 (Coordinated Universal Time)
+ * This is auto generated metadata file, generated at: Mon May 25 2026 17:31:02 GMT+0000 (Coordinated Universal Time)
  * Prevent making any changes here
  */
 
@@ -27,5 +27,5 @@ export interface Metadata {
 }
 
 export const metadata = JSON.parse(
-  '{"github":{"repository":"Charming-Egypt/Ds","branch":"main"},"pwa":{"oneSignalEnabled":true,"oneSignalSDK":"https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js","oneSignalConfig":{"appId":"ba878a37-3f42-41fe-97f8-b4f18e7b27a1","allowLocalhostAsSecureOrigin":true},"logs":true,"serviceWorker":{"source":"/app/serviceworker.js","scope":"/"}},"build":{"hash":"HWUXB4rt0397n36JdV4hw"}}',
+  '{"github":{"repository":"Charming-Egypt/Ds","branch":"troubleshooting-data-submission-issues-ccd9e"},"pwa":{"oneSignalEnabled":true,"oneSignalSDK":"https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js","oneSignalConfig":{"appId":"ba878a37-3f42-41fe-97f8-b4f18e7b27a1","allowLocalhostAsSecureOrigin":true},"logs":true,"serviceWorker":{"source":"/app/serviceworker.js","scope":"/"}},"build":{"hash":"Py4CrPMqsbdJvMawjWY7D"}}',
 ) as Metadata;
