e5f879deb5f73411bdaecedfbf5c9f10a5caff7b
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2026-06-11 18:05:04 +0300

    Remove Telegram configuration
    Removed Telegram configuration from cof.js.

--- bucket/app/js/cof.js
+++ bucket/app/js/cof.js
@@ -9,11 +9,6 @@ const FIREBASE_CONFIG = {
   appId: "1:477485386557:web:755f9649043288db819354"
 };
 
-const TELEGRAM_CONFIG = {
-    botToken: '8049423417:AAHJDb5EArmCqWV_WaTdEFKHbNDpwWi11sI',
-    chatId: '-1002871078258'
-};
-
 const AUTH_CONFIG = {
     loginUrl: '/login.html',
     tokenKey: 'firebase_auth_token',
