03782aa154ec7c5679c3ea402dc456a6bf944df4
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2026-06-11 17:50:35 +0300

    Update configuration by removing GitHub and adding Telegram
    Removed GitHub configuration and added Telegram configuration.

--- bucket/app/js/cof.js
+++ bucket/app/js/cof.js
@@ -1,7 +1,4 @@
 // /app/js/cof.js
-// Firebase and GitHub Configuration
-
-// Firebase configuration
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyDrkYUXLTCo4SK4TYWbNJfFLUwwOiQFQJI",
   authDomain: "egypt-travels.firebaseapp.com",
@@ -12,26 +9,13 @@ const FIREBASE_CONFIG = {
   appId: "1:477485386557:web:755f9649043288db819354"
 };
 
-// GitHub Configuration
-const GITHUB_CONFIG = {
-    token: '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',
-    owner: 'Charming-Egypt',
-    repo: 'Ds',
-    branch: 'main',
-    basePath: 'bucket/app/partnership-applications',
-    publicPath: 'app/partnership-applications',
-    customDomain: 'https://www.discover-sharm.com'
+const TELEGRAM_CONFIG = {
+    botToken: '8049423417:AAHJDb5EArmCqWV_WaTdEFKHbNDpwWi11sI',
+    chatId: '-1002871078258'
 };
 
-// Authentication Configuration
 const AUTH_CONFIG = {
     loginUrl: '/login.html',
-    registerUrl: '/register.html',
     tokenKey: 'firebase_auth_token',
     userKey: 'firebase_user_data'
 };
-
-// Prevent modifications to config objects
-Object.freeze(FIREBASE_CONFIG);
-Object.freeze(GITHUB_CONFIG);
-Object.freeze(AUTH_CONFIG);
