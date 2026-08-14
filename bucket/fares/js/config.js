d99904f77213771b8ed9ed2e3a654453c6bacc4b
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2026-08-15 02:35:24 +0300

    Add Firebase configuration to config.js

---
+++ bucket/fares/js/config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+var FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD96yAIaKEjOBENv26S-Y1C30MTshJd3zU",
+    authDomain: "grad-a.firebaseapp.com",
+    databaseURL: "https://grad-a-default-rtdb.firebaseio.com",
+    projectId: "grad-a",
+    storageBucket: "grad-a.firebasestorage.app",
+    messagingSenderId: "731678542675",
+    appId: "1:731678542675:web:bc3b6fbf350c5a5d392fa6"
+};
