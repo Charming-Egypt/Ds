034d562258279ebbd1aca9916637269f1aae97ce
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2026-09-11 17:55:37 +0300

    Update frontend.js

--- bucket/m/frontend.js
+++ bucket/m/frontend.js
@@ -477,7 +477,7 @@ const HERO_BACKGROUNDS = {
     ],
     mobile: [
     'https://images.unsplash.com/photo-1682687982141-0143020ed57a?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    '/m/images/home/trips/s.jpg',
+    '/m/images/home/trips/33.jpg',
     'https://res.klook.com/image/upload/fl_lossy.progressive,q_65/activities/qfewhof69v7wd1y8gas4.webp'
     ]
   }
