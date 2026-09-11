36c00e0796933034708249384818ea36af3d3fc0
Charming-Egypt <159569669+Charming-Egypt@users.noreply.github.com>
2026-09-11 16:07:49 +0300

    Update frontend.js

--- bucket/m/frontend.js
+++ bucket/m/frontend.js
@@ -479,7 +479,7 @@ const HERO_BACKGROUNDS = {
     ],
     mobile: [
     'https://images.unsplash.com/photo-1682687982141-0143020ed57a?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    '/m/images/home/trips/1.jpg',
+    'https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_863/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/qbs19tubqe67cesbzir5/ATVQuadBikeSafariinSharmElSheikh.webp',
     '/m/images/home/trips/2.jpg',
     '/m/images/home/trips/3.jpg',
     '/m/images/home/trips/4.jpg'
